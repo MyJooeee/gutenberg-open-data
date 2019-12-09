72e02da65b844580b63bd0cbc4f3d91b98d50f9a
Jonathan <dancette.jonathan@gmail.com>
2019-12-09 17:04:35 +0100

    Fix commentaire explication

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $data = ['a', 'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'c', 'c', 'c', 'c'];
 Sur un tirage aléatoire avec équiprobabilité, de manière équivalente on a aussi :
 $data = ['a', 'a', 'c', 'c', 'c', 'c', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'];
 Trié par ordre d'apparition croissant, c'est ce qui nous intèresse ici
-Equivalent à un tirage équiprobable sur 22 :
+Equivalent à un tirage équiprobable sur 14 :
 $data = [
 	'a' => 2,
 	'c' => 6,
